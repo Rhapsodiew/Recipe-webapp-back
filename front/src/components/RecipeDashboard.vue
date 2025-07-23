f73a24cebb27f3ba7b364613c67de99adfa54722
semavo_t <semavo_t@etna-alternance.net>
2025-07-23 21:24:03 +0200

    [ADD] Base front. Dashboard with list of recipes. Update on click not implemented yet.

---
+++ front/src/components/RecipeDashboard.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-table>
+        <thead>
+            <tr>
+                <th>Nom de la recette</th>
+                <th>Ingredients</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="recipe in recipes" :key="recipe.id"
+            @click="$emit('updateRecipe', recipe)"
+            style="cursor: pointer;">
+                <td>{{ recipe.name }}</td>
+                <td>
+                    <ul>
+                        <li v-for="ingredient in recipe.ingredients" :key="ingredient.id">
+                            {{ ingredient.name }}
+                        </li>
+                    </ul>
+                </td>
+            </tr>
+        </tbody>
+    </v-table>
+</template>
+
+<script setup>
+
+defineProps({
+    recipes: Array
+})
+defineEmits(['updateRecipe'])
+</script>
---
+++ front/src/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-text>
+        <RecipeDashboard :recipes="recipes" @updateRecipe="openForm" />
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+function openForm(recipe) {
+  console.log(recipe)
+}
+
+const recipes = ref([])
+
+// Fetch recipes
+onMounted(async () => {
+  try {
+    const res = await axios.get('http://localhost:8000/api/recipes')
+    recipes.value = res.data
+  } catch (err) {
+    console.error(err)
+  }
+})
+</script>
